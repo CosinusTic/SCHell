d5402d5eec3fcf21f58354fdb1a23000f3fc6531
Nathan Delmarche <delmarchenathan@gmail.com>
2025-04-02 11:35:29 +0200

    Functional window, struggling to bind to PTY

--- window/win_cpy.rs
+++ window/win_cpy.rs
@@ -1,7 +1,5 @@
 extern crate sdl2;
 
-use anyhow::{self};
-use portable_pty::{native_pty_system, CommandBuilder, PtyPair, PtySize};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -10,9 +8,6 @@ use sdl2::render::TextureCreator;
 use sdl2::render::TextureQuery;
 use sdl2::ttf::Font;
 use sdl2::video::WindowContext;
-use std::io::{stdin, stdout, Read, Write};
-use std::sync::{Arc, Mutex};
-use std::thread;
 use std::time::{Duration, Instant};
 
 const FONT_PATH: &str = "/usr/share/fonts/truetype/hack/Hack-Regular.ttf";
@@ -30,57 +25,30 @@ fn render_line<'a>(
     texture_creator.create_texture_from_surface(&surface).ok()
 }
 
-pub fn main() -> Result<(), anyhow::Error> {
-    let sdl_context = sdl2::init().unwrap();
-    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string()).unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
+    let video_subsystem = sdl_context.video()?;
     let mut cursor_visible = true;
     let mut last_cursor_toggle = Instant::now();
     let cursor_blink_interval = Duration::from_millis(500);
-    let pty_system = native_pty_system();
-    let rows: u16 = 24;
-    let cols: u16 = 80;
-
-    let p2 = "/home/nathan/Documents/Personal/Projects/my_shell/target/debug/shell";
-
-    let pair: PtyPair = pty_system.openpty(PtySize {
-        rows,
-        cols,
-        pixel_width: 0,
-        pixel_height: 0,
-    })?;
-
-    let cmd = CommandBuilder::new(p2);
-    let mut child = pair.slave.spawn_command(cmd)?;
-    // let mut writer = pair.master.take_writer()?;
-    // let mut reader = pair.master.try_clone_reader()?;
-    let mut writer = Arc::new(Mutex::new(pair.master.take_writer()?));
-    let mut reader = Arc::new(Mutex::new(pair.master.try_clone_reader()?));
-    let output_lines = Arc::new(Mutex::new(Vec::<String>::new()));
 
     let window = video_subsystem
         .window("SDL2 Terminal", 800, 600)
         .position_centered()
         .build()
-        .map_err(|e| e.to_string())
-        .unwrap();
+        .map_err(|e| e.to_string())?;
 
-    let mut canvas = window
-        .into_canvas()
-        .build()
-        .map_err(|e| e.to_string())
-        .unwrap();
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
     let texture_creator = canvas.texture_creator();
 
-    let font = ttf_context.load_font(FONT_PATH, FONT_SIZE).unwrap();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    let font = ttf_context.load_font(FONT_PATH, FONT_SIZE)?;
+    let mut event_pump = sdl_context.event_pump()?;
 
-    // let mut input_buffer = String::new();
     let mut input_buffer = String::new();
     let mut lines: Vec<String> = vec![];
     let line_height = font.recommended_line_spacing();
 
-    // FW stdin from master to slave
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -102,40 +70,9 @@ pub fn main() -> Result<(), anyhow::Error> {
                     keycode: Some(Keycode::Return),
                     ..
                 } => {
-                    let writer_clone = Arc::clone(&writer);
-                    let reader_clone = Arc::clone(&reader);
-                    let input = input_buffer.clone();
                     lines.push(input_buffer.clone());
-                    thread::spawn(move || {
-                        let mut writer = writer_clone.lock().unwrap();
-                        if writer.write_all(input.as_bytes()).is_err() {
-                            println!("Cannot write to slave (shell)")
-                        } else {
-                            println!("Successfully wrote to slave: {}", input);
-                        }
-                    });
-                    thread::spawn(move || {
-                        let mut out = stdout();
-                        let mut reader = reader_clone.lock().unwrap();
-                        let mut buf = [0u8; 1024];
-                        loop {
-                            match reader.read(&mut buf) {
-                                Ok(0) => break,
-                                Ok(n) => {
-                                    if out.write_all(&buf[..n]).is_err() {
-                                        break;
-                                    } else {
-                                        let s: String =
-                                            String::from_utf8_lossy(&buf[..n]).to_string();
-                                        println!("Recieved from slave: {}", s);
-                                        // Append to input_buffer here ?
-                                    }
-                                    let _ = out.flush();
-                                }
-                                Err(_) => break,
-                            }
-                        }
-                    });
+                    // Interpret commands here
+                    println!("Buffer: {}", input_buffer);
                     input_buffer.clear();
                 }
                 _ => {}
@@ -187,16 +124,3 @@ pub fn main() -> Result<(), anyhow::Error> {
 
     Ok(())
 }
-
-/*
-fn main() -> anyhow::Result<()> {
-
-
-
-
-    // FW stdout from slave to master
-
-    child.wait()?;
-    Ok(())
-}
-*/
